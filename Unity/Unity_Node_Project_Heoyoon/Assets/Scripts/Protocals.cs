0ba283ba3ad61ae315686c3afc58c632554dd9bb
YozoraNoShita <junsu071802@gmail.com>
2023-09-13 12:02:12 +0900

    Feat: "서버 포트 3030에 GET,POST 유니티 테스트"
    데이터 입력, 데이터 전송 확인

---
+++ Unity/Unity_Node_Project_Heoyoon/Assets/Scripts/Protocals.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Protocals : MonoBehaviour
+{
+    public class Packets
+    {
+        public int adf;
+        public class common
+        {
+            public int cmd;                     //커맨드 번호
+            public string message;      //메세지
+        }
+        public class req_data : common  //Common 상속
+        {
+            public int id;
+            public string data;
+        }
+    }
+}
